118111eeca44b56a7b0a7534acf63387aa22791b
d.halitski <d.halitski@gmail.com>
2016-08-14 00:04:48 +0300

    MultiGeometry implementation Styles implementation

--- bingKmlParser.js
+++ bingKmlParser.js
@@ -1,16 +1,19 @@
 var bingKmlParser = function () {
 
+    var ICON_URL = 'https://www.bingmapsportal.com/Content/images/poi_custom.png';
+
     var styles = {};
     var styleMap = {};
 
     /**
      * @param placemarkXml
-     * @returns {*}
+     * @returns {Array}
      */
     function parsePlacemark(placemarkXml) {
         const placemarkDom = $(placemarkXml);
 
         var geometries = [];
+
         var pushpins = parsePushpin(placemarkDom);
         if (pushpins) {
             geometries.push(pushpins);
@@ -18,116 +21,160 @@ var bingKmlParser = function () {
 
         var lines = parseLine(placemarkDom);
         if (lines) {
-            geometries.push(pushpins);
+            geometries.push(lines);
         }
 
         var polygons = parsePolygon(placemarkDom);
         if (polygons) {
-            geometries.push(pushpins);
+            geometries.push(polygons);
         }
 
-        geometries = parseMultiGeometry(placemarkDom);
-        if (geometries) {
-
+        var multiGeometries = parseMultiGeometry(placemarkDom);
+        if (multiGeometries) {
+            $.each(multiGeometries, function (index, item) {
+                geometries.push(item);
+            });
         }
 
         return geometries;
     }
 
-    function parsePushpin(placemarkDom) {
-        var point = placemarkDom.find('Point');
-        if (point.length === 0) {
-            return null;
-        }
-        var coordinates = point.find('coordinates');
-        var vertices = parseVertices(coordinates.firstChild.data);
+    function parsePushpin(placemarkDom, pointDom) {
+        var point;
 
-        var pushpinOptions;
-
-        const styleUrl = placemarkDom.find('styleUrl');
-        if (styleUrl.length > 0) {
-            var colorName = styleUrl.html().substr(1);
-            pushpinOptions = styles[colorName];
-        } else {
-            var style = placemarkDom.find('Style');
-            pushpinOptions = parseStyle(style);
-        }
-
-        var name = placemarkDom.find('name');
-        if (name.length > 0) {
-            pushpinOptions.name = name.html();
-        }
-
-        return new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(vertices[0], vertices[1]), pushpinOptions);
-    }
-
-    function parseLine(placemarkDom) {
-        var line = placemarkDom.find('LineString');
-        if (line.length === 0) {
-            return null;
-        }
-
-        var coordinates = line.find('coordinates');
-        var vertices = parseVertices(coordinates.firstChild.data);
-
-        var lineOptions;
-
-        const styleUrl = placemarkDom.find('styleUrl');
-        if (styleUrl.length > 0) {
-            var colorName = styleUrl.contents()[0].data.substr(1);
-            lineOptions = styleMap[colorName];
-            if (!lineOptions) {
-                lineOptions = styles[colorName];
+        try {
+            if (pointDom) {
+                point = pointDom
+            } else {
+                point = placemarkDom.find('Point');
+                if (point.length === 0) {
+                    return null;
+                }
             }
-        } else {
-            var style = placemarkDom.find('Style');
-            lineOptions = parseStyle(style);
-        }
 
-        return new Microsoft.Maps.Polyline(vertices, lineOptions);
-    }
+            var coordinates = point.find('coordinates');
+            var vertices = parseVertices(coordinates.html());
 
-    function parsePolygon(placemarkDom) {
-        var polygon = placemarkDom.find('Polygon');
-        if (polygon.length === 0) {
-            return null;
-        }
-        var coordinates = polygon.find('coordinates');
-        var vertices = parseVertices(coordinates.firstChild.data);
-        var polygonOptions;
+            var pushpinOptions = {icon: ICON_URL, anchor: new Microsoft.Maps.Point(12, 39)};
 
-        const styleUrl = placemarkDom.find('styleUrl');
-        if (styleUrl.length > 0) {
-            var colorName = styleUrl.contents()[0].data.substr(1);
-            polygonOptions = styleMap[colorName];
-            if (!polygonOptions) {
-                polygonOptions = styles[colorName];
+            if (placemarkDom) {
+                const styleUrl = placemarkDom.find('styleUrl');
+                if (styleUrl.length > 0) {
+                    var colorName = styleUrl.html().substr(1);
+                    pushpinOptions = styles[colorName];
+                } else {
+                    var style = placemarkDom.find('Style');
+                    pushpinOptions = parsePushPinStyle(style);
+                }
+
+                var name = placemarkDom.find('name');
+                if (name.length > 0) {
+                    console.log(name.html());
+                    pushpinOptions.name = name.html();
+                }
             }
-        } else {
-            var style = placemarkDom.find('Style');
-            polygonOptions = parseStyle(style);
-        }
 
-        return new Microsoft.Maps.Polygon(vertices, polygonOptions);
+            return new Microsoft.Maps.Pushpin(vertices[0], pushpinOptions);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
-    function parseMultiGeometry(multiGeometryDom) {
-        var polygon = multiGeometryDom.find('MultiGeometry');
-        if (polygon.length === 0) {
+    function parseLine(placemarkDom, lineDom) {
+        try {
+            var line;
+            if (lineDom) {
+                line = lineDom
+            } else {
+                line = placemarkDom.find('LineString');
+                if (line.length === 0) {
+                    return null;
+                }
+            }
+
+            var coordinates = line.find('coordinates');
+            var vertices = parseVertices(coordinates.html());
+
+            var lineOptions = {
+                strokeColor: 'black', strokeThickness: 1
+            };
+
+            if (placemarkDom) {
+                const styleUrl = placemarkDom.find('styleUrl');
+                if (styleUrl.length > 0) {
+                    var colorName = styleUrl.html().substr(1);
+                    lineOptions = styleMap[colorName];
+                    if (!lineOptions) {
+                        lineOptions = styles[colorName];
+                    }
+                } else {
+                    var style = placemarkDom.find('Style');
+                    lineOptions = parsePolylineStyle(style);
+                }
+            }
+
+            return new Microsoft.Maps.Polyline(vertices, lineOptions);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function parsePolygon(placemarkDom, polygonDom) {
+        try {
+            var polygon;
+            if (polygonDom) {
+                polygon = polygonDom
+            } else {
+                polygon = placemarkDom.find('Polygon');
+                if (polygon.length === 0) {
+                    return null;
+                }
+            }
+            var coordinates = polygon.find('coordinates');
+            var vertices = parseVertices(coordinates.html());
+            var polygonOptions = {
+                fillColor: null,
+                strokeColor: 'black',
+                strokeThickness: 1
+            };
+
+            if (placemarkDom) {
+                const styleUrl = placemarkDom.find('styleUrl');
+                if (styleUrl.length > 0) {
+                    var colorName = styleUrl.html().substr(1);
+                    polygonOptions = styleMap[colorName];
+                    if (!polygonOptions) {
+                        polygonOptions = styles[colorName];
+                    }
+                } else {
+                    var style = placemarkDom.find('Style');
+                    polygonOptions = parsePolygonStyle(style);
+                }
+            }
+
+            return new Microsoft.Maps.Polygon(vertices, polygonOptions);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function parseMultiGeometry(placemarkDom) {
+        var multiGeometryDom = placemarkDom.find('MultiGeometry');
+        if (multiGeometryDom.length === 0) {
             return null;
         }
 
         var geometries = [];
         multiGeometryDom.find('Point').each(function (index, item) {
-            geometries.push(parsePushpin(item));
+            geometries.push(parsePushpin(null, item));
         });
 
         multiGeometryDom.find('LineString').each(function (index, item) {
-            geometries.push(parseLine(item));
+            geometries.push(parseLine(null, item));
         });
 
         multiGeometryDom.find('Polygon').each(function (index, item) {
-            geometries.push(parsePolygon(item));
+            geometries.push(parsePolygon(null, item));
         });
 
         return geometries;
@@ -175,13 +222,13 @@ var bingKmlParser = function () {
     }
 
     function parseStyle(styleDom) {
-        var msPolygonOptions = {};
+        var styleOptions = {};
 
         const polyStyle = styleDom.find('PolyStyle');
         if (polyStyle.length > 0) {
             const color = polyStyle.find('color');
             if (color.length > 0) {
-                msPolygonOptions.fillColor = parseColor(color.html());
+                styleOptions.fillColor = parseColor(color.html());
             }
         }
 
@@ -189,30 +236,99 @@ var bingKmlParser = function () {
         if (lineStyle.length > 0) {
             const color = lineStyle.find('color');
             if (color.length > 0) {
-                msPolygonOptions.strokeColor = parseColor(color.html());
+                styleOptions.strokeColor = parseColor(color.html());
             }
             const width = lineStyle.find('width');
             if (width.length > 0) {
-                msPolygonOptions.strokeThickness = parseInt(width.html());
+                styleOptions.strokeThickness = parseInt(width.html());
             }
         }
 
         const iconStyle = styleDom.find('IconStyle');
         if (iconStyle.length > 0) {
-            msPolygonOptions.icon = {};
-
             var color = iconStyle.find('color');
             if (color.length > 0) {
-                msPolygonOptions.icon.color = color.html();
+                styleOptions.color = color.html();
             }
 
             var icon = iconStyle.find('Icon');
             if (icon.length > 0) {
-                msPolygonOptions.icon.href = icon.find('href').html();
+                styleOptions.icon = icon.find('href').html();
             }
 
         }
-        return msPolygonOptions;
+        return styleOptions;
+    }
+
+    function parsePushPinStyle(styleDom) {
+        var styleOptions = {
+            icon: ICON_URL
+        };
+
+        const iconStyle = styleDom.find('IconStyle');
+        if (iconStyle.length > 0) {
+            var color = iconStyle.find('color');
+            if (color.length > 0) {
+                styleOptions.color = color.html();
+            }
+
+            var icon = iconStyle.find('Icon');
+            if (icon.length > 0) {
+                styleOptions.icon = icon.find('href').html();
+            }
+
+        }
+        return styleOptions;
+    }
+
+    function parsePolygonStyle(styleDom) {
+        var styleOptions = {
+            fillColor: null,
+            strokeColor: 'black',
+            strokeThickness: 1
+        };
+
+        const polyStyle = styleDom.find('PolyStyle');
+        if (polyStyle.length > 0) {
+            const color = polyStyle.find('color');
+            if (color.length > 0) {
+                styleOptions.fillColor = parseColor(color.html());
+            }
+        }
+
+        const lineStyle = styleDom.find('LineStyle');
+        if (lineStyle.length > 0) {
+            const color = lineStyle.find('color');
+            if (color.length > 0) {
+                styleOptions.strokeColor = parseColor(color.html());
+            }
+            const width = lineStyle.find('width');
+            if (width.length > 0) {
+                styleOptions.strokeThickness = parseInt(width.html());
+            }
+        }
+
+        return styleOptions;
+    }
+
+    function parsePolylineStyle(styleDom) {
+        var styleOptions = {
+            strokeColor: 'black', strokeThickness: 1
+        };
+
+        const lineStyle = styleDom.find('LineStyle');
+        if (lineStyle.length > 0) {
+            const color = lineStyle.find('color');
+            if (color.length > 0) {
+                styleOptions.strokeColor = parseColor(color.html());
+            }
+            const width = lineStyle.find('width');
+            if (width.length > 0) {
+                styleOptions.strokeThickness = parseInt(width.html());
+            }
+        }
+
+        return styleOptions;
     }
 
     /**
@@ -236,6 +352,8 @@ var bingKmlParser = function () {
         //
         //    styleMap[styleMapId] = styles[mappedStyle];
         //});
+
+        console.log('Styles loaded');
     }
 
     /**
@@ -243,18 +361,22 @@ var bingKmlParser = function () {
      */
     var _public = {
         parse: function (kmlXml) {
-            var polygons = [];
+            var kmlContent = [];
 
             parseStyles(kmlXml);
 
             $(kmlXml).find('Placemark').each(function (index, placemarkXml) {
-                const polygon = parsePlacemark(placemarkXml);
-                if (polygon) {
-                    polygons.push(polygon);
+                const placemarkGeometries = parsePlacemark(placemarkXml);
+                if (placemarkGeometries) {
+                    $.each(placemarkGeometries, function (index, item) {
+                        kmlContent.push(item);
+                    });
                 }
             });
 
-            return polygons;
+            console.log('Geometry loaded');
+
+            return kmlContent;
         }
     };
 
